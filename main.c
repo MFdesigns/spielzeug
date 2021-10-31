06d4bd066de8fe4c93dc090265d5cdf414003e8b
MFdesigns <michel.faeh@stafag.ch>
2021-10-31 19:32:29 +0100

    Added script for bootable image creation

--- main.c
+++ main.c
@@ -3,5 +3,8 @@
 #include "efi.h"
 
 EFI_STATUS efi_main(EFI_HANDLE imageHandle, EFI_SYSTEM_TABLE* systemTable) {
+    systemTable->conOut->outputString(systemTable->conOut, L"Hello World\r\n");
+    
+    while(true) {};
     return EFI_SUCCESS;
 }

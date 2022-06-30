8f7aa2303560750cd8dcd37adece97d32f68f368
MFdesigns <michel.faeh@stafag.ch>
2022-06-30 20:15:15 +0200

    Wrapped formatted print in macro helper

--- src/print.h
+++ src/print.h
@@ -17,11 +17,19 @@ void copyMemory(u8* dest, u8* src, u32 size) {
     }
 }
 
-void uintToString(char16 destination[], u64 num) {
-    const u32 charSize = sizeof(destination[0]);
+typedef void (*PutCharFunc) (void* instance, char c);
 
-    char16 buffer[INT64_MAX_DIGITS + 1]; // Plus null terminator
-    setMemory((u8*)buffer, (INT64_MAX_DIGITS + 1) * charSize, 0);
+void* globalPrintInstance = 0;
+PutCharFunc globalPutChar = 0;
+
+void formatInit(void* instance, PutCharFunc func) {
+    globalPrintInstance = instance;
+    globalPutChar = func;
+}
+
+void fmtUintToString(u64 num) {
+    char buffer[INT64_MAX_DIGITS + 1]; // Plus null terminator
+    setMemory((u8*)buffer, (INT64_MAX_DIGITS + 1), 0);
 
     s32 firstDigit = -1;
 
@@ -44,40 +52,156 @@ void uintToString(char16 destination[], u64 num) {
     }
     
     u32 digitCount = (INT64_MAX_DIGITS + 1) - firstDigit;
-    
-    copyMemory((u8*)destination, (u8*)&buffer[firstDigit], digitCount * charSize);
+    for (u32 i = 0; i < digitCount; i++) {
+        globalPutChar(globalPrintInstance, (char)buffer[firstDigit + i]);
+    }
 }
 
-void uintToHexString(char16 destination[], u64 num) {
-    u32 charSize = sizeof(destination[0]);
-
-    char16 buffer[INT64_MAX_HEX_DIGITS + 1];
-    u32 bufferSize = (INT64_MAX_HEX_DIGITS + 1) * sizeof(buffer[0]);
-    setMemory((u8*)buffer, bufferSize, 0);
-
-    u32 outIndex = INT64_MAX_HEX_DIGITS - 1;
-
-    if (num == 0) {
-        buffer[outIndex] = '0';
-        outIndex--;
-    } else {
-        char hexUnicodeTable[16] = {
+void fmtUintToHexString(u64 num, bool upperCase) {
+    char hexTableUpper[16] = {
         '0', '1', '2', '3',
         '4', '5', '6', '7',
         '8', '9', 'A', 'B',
         'C', 'D', 'E', 'F'
-        };
+    };
+
+    char hexTableLower[16] = {
+        '0', '1', '2', '3',
+        '4', '5', '6', '7',
+        '8', '9', 'a', 'b',
+        'c', 'd', 'e', 'f'
+    };
+
+    char* hexTable = hexTableLower;
+    if (upperCase) {
+        hexTable = hexTableUpper;
+    }
+
+    if (num == 0) {
+        globalPutChar(globalPrintInstance, '0');
+    } else {
 
         u64 remainder = num % 16;
         while (num != 0) {
             u64 quotient = num / 16;
-            buffer[outIndex] = hexUnicodeTable[remainder];
+            globalPutChar(globalPrintInstance, hexTable[remainder]);
             num = quotient;
             remainder = num % 16;
-            outIndex--;
         }
     }
+}
 
-    u32 stringSizeBytes = bufferSize - (outIndex + 1) * charSize;
-    copyMemory((u8*)destination, (u8*)&buffer[outIndex + 1], stringSizeBytes);
+enum FormatArgType {
+    ARG_U8,
+    ARG_U16,
+    ARG_U32,
+    ARG_U64
+};
+
+struct FormatArg {
+    u64 type;
+    u64 data;
+};
+
+// NOTE: not really sure why we need to do this but if we dont then
+// __COUNTER__ will not get replaced...
+#define internalFormatMacro2(str, counter, ...) \
+    const char tmp_fmt_str_ ## counter[] = str; \
+    struct FormatArg tmp_fmt_args_ ## counter [] = { __VA_ARGS__ }; \
+    internalPrintFormat(tmp_fmt_str_ ## counter, sizeof(tmp_fmt_str_ ## counter), tmp_fmt_args_ ## counter, sizeof(tmp_fmt_args_ ## counter) / sizeof(tmp_fmt_args_ ## counter[0]))
+
+#define internalFormatMacro(str, counter, ...) \
+    internalFormatMacro2(str, counter, __VA_ARGS__)
+
+#define format(str, ...) \
+    internalFormatMacro(str, __COUNTER__, __VA_ARGS__)
+
+#define fmt(var) { _Generic(var, u8: ARG_U8, u16: ARG_U16, u32: ARG_U32, u64: ARG_U64), var }
+
+struct FormatStringParser {
+    const char* string;
+    u32 stringSize;
+    u32 cursor;
+};
+
+static char fmtPeek(struct FormatStringParser* parser) {
+    if (parser->cursor >= parser->stringSize) {
+        return 0;
+    }
+    return parser->string[parser->cursor];
+}
+
+static char fmtConsume(struct FormatStringParser* parser) {
+    if (parser->cursor + 1 >= parser->stringSize) {
+        return 0;
+    }
+
+    char c = parser->string[parser->cursor];
+    parser->cursor += 1;
+    return c;
+}
+
+#define FMT_SPEC_DECIMAL        0
+#define FMT_SPEC_HEX_UPPERCASE  1
+#define FMT_SPEC_HEX_LOWERCASE  2
+
+struct FormatSpecifier {
+    u32 flags;
+};
+
+void fmtParseSpecifier(
+    struct FormatStringParser* parser, 
+    struct FormatSpecifier* outSpecifier
+) {
+    char c = fmtConsume(parser);
+    while (c != 0 && c != '}') {
+        switch (c) {
+        case 'X': outSpecifier->flags |= FMT_SPEC_HEX_UPPERCASE; break;
+        case 'x': outSpecifier->flags |= FMT_SPEC_HEX_LOWERCASE; break;
+        default: break;
+        }
+        c = fmtConsume(parser);
+    }
+}
+
+void internalPrintFormat(const char* format, u32 formatSize, struct FormatArg* args, u32 argCount) {
+    struct FormatStringParser parser = {
+        .string = format,
+        .stringSize = formatSize,
+        .cursor = 0,
+    };
+
+    u32 argIndex = 0;
+
+    char c = fmtConsume(&parser);
+    while (c) {
+        if (c == '{') {
+            struct FormatSpecifier specifier = {
+                .flags = 0
+            };
+            fmtParseSpecifier(&parser, &specifier);
+
+            if (argIndex < argCount) {
+                struct FormatArg* arg = &args[argIndex];
+                u64 data = 0;
+                switch (specifier.flags) {
+                case FMT_SPEC_DECIMAL:
+                    fmtUintToString(arg->data);
+                    break;
+                case FMT_SPEC_HEX_UPPERCASE:
+                    fmtUintToHexString(arg->data, true);
+                    break;
+                case FMT_SPEC_HEX_LOWERCASE:
+                    fmtUintToHexString(arg->data, false);
+                    break;
+                }
+
+                argIndex++;
+            }
+        } else {
+            globalPutChar(globalPrintInstance, c);
+        }
+
+        c = fmtConsume(&parser);
+    }
 }
--- src/stdlib_test.c
+++ src/stdlib_test.c
@@ -1,38 +1,16 @@
-#include "stdio.h"
 // stdlib headers
 #include "print.h"
 #include "types.h"
+#include "stdio.h"
+
+void putChar(void* instance, char c) {
+    printf("%c", c);
+}
 
 int main() {
-    u64 num1 = 2;
-    u64 num2 = 1234567890;
-    u64 num3 = 0xFFFFFFFFFFFFFFFF;
+    formatInit(0, putChar);
 
-    char16 output[256];
-
-    // Decimal numbers
-    uintToString(output, 0);
-    printf("Num %llu: '%ls'\n", 0ULL, output);
-
-    uintToString(output, num1);
-    printf("Num %llu: '%ls'\n", num1, output);
-
-    uintToString(output, num2);
-    printf("Num %llu: '%ls'\n", num2, output);
-
-    uintToString(output, num3);
-    printf("Num %llu: '%ls'\n", num3, output);
-
-    // Hex numbers
-    uintToHexString(output, 0);
-    printf("Num %llx: '%ls'\n", 0ULL, output);
-
-    uintToHexString(output, num1);
-    printf("Num %llx: '%ls'\n", num1, output);
-
-    uintToHexString(output, num2);
-    printf("Num %llx: '%ls'\n", num2, output);
-
-    uintToHexString(output, num3);
-    printf("Num %llx: '%ls'\n", num3, output);
+    format("Num 1: {}\n", fmt((u32)1));
+    format("Num 0xFF: 0x{X}\n", fmt((u8)255));
+    format("Num 0xff: 0x{x}\n", fmt((u8)255));
 }
